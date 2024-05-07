47858558dd85a6f538b75df1025f7940034aaf9c
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-07 14:35:31 +0430

    add class categroy and blogpost

--- sign/models.py
+++ sign/models.py
@@ -8,6 +8,27 @@ class User(models.Model):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     registration_date = models.DateTimeField(auto_now_add=True)
     role = models.CharField(max_length=50, default='user')
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
-      return self.username
+        return self.username
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class BlogPost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
