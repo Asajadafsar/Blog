83735fc2ded4bdd6c84b41f0376a442ba068d400
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-08 01:40:11 +0430

    add comment

--- sign/admin.py
+++ sign/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from sign.models import User
+from sign.models import User,Category,BlogPost,Comment
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(BlogPost)
+admin.site.register(Comment)
