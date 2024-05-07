22513f580232fa805d50f1c9a0f56acadd9f8d91
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-07 11:13:47 +0430

    add login path

--- sign/urls.py
+++ sign/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, login_user
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
+    path('login/', login_user, name='login_user'),
+    # path('edit/', edit_profile, name='edit_profile'),
 ]
