010bfefb4546d36fcd59500868911ca26efeb959
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-07 11:43:49 +0430

    add path view and edit profile and rest password

--- sign/urls.py
+++ sign/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import register_user, login_user
+from .views import register_user, login_user,profile,edit_profile,reset_password
 
 urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('login/', login_user, name='login_user'),
-    # path('edit/', edit_profile, name='edit_profile'),
+    path('profile/', profile, name='profile'),
+    path('profile/edit', edit_profile, name='edit_profile'),
+    path('rest', reset_password, name='reset_password'),
+
 ]
