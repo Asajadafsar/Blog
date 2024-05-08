12a853eaa582378e7a92635ba59a5d41642966f0
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-08 14:20:08 +0430

    veiw defult admin Django

--- sign/admin.py
+++ sign/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from sign.models import User,Category,BlogPost,Comment
+from sign.models import User,Category,BlogPost,Comment,Tag
 
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(BlogPost)
 admin.site.register(Comment)
+admin.site.register(Tag)
