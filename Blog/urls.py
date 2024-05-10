e5f589f03214fe66d0471b1f939b697c1aeb4270
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-11 03:59:14 +0430

    add all files

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sign/', include('sign.urls')), 
+    path('admin_view/', include('admin_view.urls')), 
+
 ]
