5a7b1d91a9bbf24f9d2534a743dbcfa1b3970d94
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-12 02:17:12 +0430

    add CRud post

--- admin_view/views.py
+++ admin_view/views.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 from functools import wraps
 import jwt
 from .models import AdminLogs
-from sign.models import User
+from sign.models import User,BlogPost
 from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
@@ -269,6 +269,41 @@ def update_user(request, user_id):
         return JsonResponse({'error': 'Method not allowed'}, status=405)
 
 
+
+#get user/id
+@csrf_exempt
+@token_required
+def get_user(request, user_id):
+    if request.method == 'GET':
+        # Ensure the logged-in user has admin role
+        if request.user.role != 'admin':
+            return JsonResponse({'error': 'Insufficient privileges'}, status=403)
+
+        # Retrieve the user by user_id
+        try:
+            user = User.objects.get(user_id=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404)
+
+        # Serialize user data
+        user_data = {
+            'id': user.user_id,
+            'username': user.username,
+            'email': user.email,
+            'phone_number': user.phone_number,
+            'role': user.role,
+            'registration_date': user.registration_date.strftime('%Y-%m-%d') if user.registration_date else None,
+            'profile_picture': request.build_absolute_uri(user.profile_picture.url) if user.profile_picture else None
+        }
+
+        return JsonResponse(user_data, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+
+
+
 #view logs
 @csrf_exempt
 @token_required
@@ -331,4 +366,198 @@ def delete_admin_log(request, log_id):
     create_adminlogs(request.user, 'Deleted log', request.META.get('REMOTE_ADDR'))
 
     return JsonResponse({'message': 'Log deleted successfully'}, status=200)
+
+
+
+
+
+#get list all posts
+def get_blog_posts(request):
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+    # Parse request parameters
+    sort_field, sort_order = request.GET.get('sort', 'post_id,ASC').split(',')
+    filter_query = request.GET.get('filter', '{}')
+    range_header = request.headers.get('Range', '0-9')
+
+    # Process sort parameters
+    if sort_field == 'id':
+        sort_field = 'post_id'
+    if sort_order not in ['ASC', 'DESC']:
+        sort_order = 'ASC'  # Default sorting order
+
+    # Process range parameters
+    start, end = map(int, range_header.split('-'))
+
+    # Build Q objects for filtering
+    q_objects = Q()
+    if filter_query:
+        filter_dict = eval(filter_query)  # Convert filter string to dictionary
+        if 'title' in filter_dict:
+            q_objects &= Q(title__icontains=filter_dict['title'])
+        if 'user__username' in filter_dict:
+            q_objects &= Q(user__username__icontains=filter_dict['user__username'])
+        if 'category__name' in filter_dict:
+            q_objects &= Q(category__name__icontains=filter_dict['category__name'])
+
+    # Perform the filtered query
+    blog_posts_query = BlogPost.objects.filter(q_objects)
+
+    # Apply sorting order
+    sort_field = sort_field if sort_order == 'ASC' else f'-{sort_field}'  # Construct field name with sorting order
+    blog_posts_query = blog_posts_query.order_by(sort_field)
+
+    # Perform pagination
+    paginator = Paginator(blog_posts_query, end - start + 1)
+    blog_posts_page = paginator.get_page(start // (end - start + 1) + 1)
+
+    # Serialize blog post data
+    blog_posts_data = [{
+        'id': post.post_id,
+        'title': post.title,
+        'image': request.build_absolute_uri(post.image.url) if post.image else None,
+        'created_at': post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+        'updated_at': post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+        'user_id': post.user.user_id,
+        'category_id': post.category.category_id,
+        'status': post.status
+    } for post in blog_posts_page]
+
+    # Prepare response
+    response = JsonResponse(blog_posts_data, status=200, safe=False)
+    response['X-Total-Count'] = paginator.count
+    response['Content-Range'] = f'blog_posts {start}-{end}/{paginator.count}'
+
+    return response
+
+
+
+#get post id
+@csrf_exempt
+@token_required
+def get_blog_post(request, post_id):
+    if request.method == 'GET':
+        # Ensure the logged-in user has admin role
+        if request.user.role != 'admin':
+            return JsonResponse({'error': 'Insufficient privileges'}, status=403)
+
+        try:
+            # Retrieve the blog post by post_id
+            post = BlogPost.objects.get(post_id=post_id)
+        except BlogPost.DoesNotExist:
+            return JsonResponse({'error': 'Blog post not found'}, status=404)
+
+        # Serialize blog post data
+        post_data = {
+            'id': post.post_id,
+            'title': post.title,
+            'content': post.content,
+            'image': request.build_absolute_uri(post.image.url) if post.image else None,
+            'created_at': post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'user_id': post.user.user_id,
+            'category_id': post.category.category_id,
+            'status': post.status
+        }
+
+        return JsonResponse(post_data, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+
+#delete post
+@csrf_exempt
+@token_required
+def delete_blog_post(request, post_id):
+    if request.method == 'DELETE':
+        # Ensure the logged-in user has admin role
+        if request.user.role != 'admin':
+            return JsonResponse({'error': 'Insufficient privileges'}, status=403)
+        
+        # Check if the blog post exists
+        post = BlogPost.objects.filter(post_id=post_id).first()
+        if not post:
+            return JsonResponse({'error': 'Blog post not found'}, status=404)
+        
+        # Delete the blog post's image file if it exists
+        if post.image:
+            image_path = os.path.join(settings.MEDIA_ROOT, str(post.image))
+            if os.path.exists(image_path):
+                os.remove(image_path)
+
+        # Delete the blog post from the database
+        post.delete()
+        # Get the IP address of the user who deleted the blog post
+        ip_address = request.META.get('REMOTE_ADDR')
+        # Create admin log
+        create_adminlogs(user=request.user, action='delete blog post', ip_address=ip_address)
+        return JsonResponse({'message': 'Blog post deleted successfully', 'id': post_id}, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+
+
+#edit post
+@csrf_exempt
+@token_required
+def update_blog_post(request, post_id):
+    if request.method == 'POST':
+        # Ensure the logged-in user has admin role
+        if request.user.role != 'admin':
+            return JsonResponse({'error': 'Insufficient privileges'}, status=403)
+
+        # Retrieve the blog post to update
+        try:
+            post = BlogPost.objects.get(pk=post_id)
+        except BlogPost.DoesNotExist:
+            return JsonResponse({'error': 'Blog post not found'}, status=404)
+
+        # Parse request data
+        data = request.POST
+
+        # Update blog post fields if provided in the request
+        post.title = data.get('title', post.title)
+        post.content = data.get('content', post.content)
+        post.status = data.get('status', post.status)
+
+        # Update the image if provided
+        if 'image' in request.FILES:
+            image_file = request.FILES['image']
+
+            # Delete previous image if exists
+            if post.image:
+                file_path = os.path.join(settings.MEDIA_ROOT, post.image.name)
+                os.remove(file_path)
+
+            # Save the new image to the media directory
+            file_path = os.path.join(settings.MEDIA_ROOT, 'blog_images', image_file.name)
+            with open(file_path, 'wb+') as destination:
+                for chunk in image_file.chunks():
+                    destination.write(chunk)
+
+            # Update the blog post's image path
+            post.image = 'blog_images/' + image_file.name
+
+        # Save the updated blog post
+        post.save()
+        # Get the IP address of the user who added the new user
+        ip_address = request.META.get('REMOTE_ADDR')
+        # Create admin log
+        create_adminlogs(user=request.user, action='edit post', ip_address=ip_address)
+        return JsonResponse({
+            'id': post.post_id,
+            'title': post.title,
+            'content': post.content,
+            'image': request.build_absolute_uri(post.image.url) if post.image else None,
+            'created_at': post.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'updated_at': post.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+            'user_id': post.user.user_id,
+            'category_id': post.category.category_id,
+            'status': post.status
+        }, status=200)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+
 
