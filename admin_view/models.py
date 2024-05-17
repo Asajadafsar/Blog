e5016d8f45e10316575d38063ff029513de05c0a
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-17 17:48:59 +0430

    add logs deelte user

--- admin_view/models.py
+++ admin_view/models.py
@@ -4,9 +4,8 @@ from sign.models import User
 
 class AdminLogs(models.Model):
     log_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     action = models.CharField(max_length=100)
     action_date = models.DateTimeField()
     ip_address = models.CharField(max_length=50)
     def __str__(self):
-        return self.user
+        return self.ip_address
