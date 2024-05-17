3b3c7a17d0c752ee692211086c44d4f40e1a8529
Asajadafsar <smyhqasmyan2@gmail.com>
2024-05-17 17:49:18 +0430

    add corse

--- Blog/settings.py
+++ Blog/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'sign',
     'admin_view',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -52,6 +54,15 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Blog.urls'
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://192.168.4.201:5173',
+]
+CORS_ALLOW_HEADERS = [
+    'barber',
+    'content-type',  # Add other allowed headers as needed
+    'authorization',
+]
 
 TEMPLATES = [
     {
